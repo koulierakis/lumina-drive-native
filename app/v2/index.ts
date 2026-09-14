0802afb7478aa4e9b61b55e07ac16949ca460b70
koulierakis <koulierakis@jsaglobal.gr>
2026-09-15 00:14:01 +0300

    feat(v2): export Valhalla routing provider

--- app/v2/index.ts
+++ app/v2/index.ts
@@ -7,6 +7,7 @@ export * from './offline-regions-store';
 export * from './offline-region-controller';
 export * from './offline-routing-packages';
 export * from './routing-package-selector';
+export * from './valhalla-offline-routing-provider';
 export * from './legacy-mapbox-providers';
 export * from './expo-voice-provider';
 export * from './lane-guidance';
